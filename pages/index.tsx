1d18f597ee4101f8e3e4c45e9707f426cc154832
mohsinraz3 <mohsinraz3@gmail.com>
2023-02-02 23:53:34 +0500

    pricing ui

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+import { Box } from "@chakra-ui/react";
+
+const IndexPage = () => (
+  <div>
+    <Header />
+  </div>
+);
+
+export default IndexPage;
