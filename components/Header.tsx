45861198c44b141c16799bcffd71860d51790bf5
mohsinraz3 <mohsinraz3@gmail.com>
2023-02-11 18:04:56 +0500

    responsiveness

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,12 +10,12 @@ export default function Header() {
         pt="90px"
         pb="198px"
         px="8"
-        textAlign={"center"}
+        textAlign={["left", "left", "center"]}
       >
-        <Heading fontWeight="extrabold" fontSize="5xl">
+        <Heading fontWeight="extrabold" fontSize={["3xl", "3xl", "5xl"]}>
           Simple pricing for your busiess
         </Heading>
-        <Text fontWeight="medium" fontSize="2xl" pt="4">
+        <Text fontWeight="medium" fontSize={["lg", "lg", "2xl"]} pt="4">
           Plans that are carefully crafted to suit your business
         </Text>
       </Box>
--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -16,50 +16,53 @@ import { CheckIcon } from "../icons/icon";
 export const ListItem = (props: StackProps) => {
   const { children, ...rest } = props;
   return (
-    <HStack as="li" spacing="20px" {...rest}>
+    <HStack as="li" spacing="20px" {...rest} alignItems="start">
       <Icon as={CheckIcon} w="22px" h="22px" />
-      <Text>{children}</Text>
+      <Text textAlign={["left", "left", "center"]}>{children}</Text>
     </HStack>
   );
 };
 
 export default function Pricing() {
   return (
-    <Box
-      maxW="994px"
-      margin="auto"
-      mt="-256px"
-      borderRadius="12px"
-      overflow="hidden"
-      boxShadow="0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)"
-    >
-      <Flex>
-        <Box bg="#F0EAFB" p="60px">
-          <Text fontSize="24px" fontWeight="800">
-            Premium PRO
-          </Text>
-          <Heading as="h3" mt="16px" fontSize="60px">
-            $329
-          </Heading>
-          <Text color="#171923" fontSize="18px" mt="8px" fontWeight="500">
-            Billed just once
-          </Text>
-          <Button colorScheme="purple" size="lg" w="282px" mt="24px">
-            Getting Started
-          </Button>
-        </Box>
-        <Box p="60px" fontSize="18px" bg="white">
-          <Text textAlign="left">
-            Access these features when you get this pricing package business.
-          </Text>
-          <Stack as="ul" spacing="20px" pt="24px">
-            <ListItem>International calling and message API</ListItem>
-            <ListItem> Additional phone numbers</ListItem>
-            <ListItem>Automated messages via Zapier</ListItem>
-            <ListItem>24/7 support and consulting</ListItem>
-          </Stack>
-        </Box>
-      </Flex>
+    <Box mx="6">
+      <Box
+        as="section"
+        maxW="994px"
+        margin="auto"
+        mt="-256px"
+        borderRadius="12px"
+        overflow="hidden"
+        boxShadow="0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04)"
+      >
+        <Flex direction={["column", "column", "row"]}>
+          <Box bg="#F0EAFB" p="60px">
+            <Text fontSize="24px" fontWeight="800">
+              Premium PRO
+            </Text>
+            <Heading as="h3" mt="16px" fontSize={["5xl", "5xl", "6xl"]}>
+              $329
+            </Heading>
+            <Text color="#171923" fontSize="18px" mt="8px" fontWeight="500">
+              Billed just once
+            </Text>
+            <Button colorScheme="purple" size="lg" w="282px" mt="24px">
+              Getting Started
+            </Button>
+          </Box>
+          <Box p="60px" fontSize="18px" bg="white">
+            <Text textAlign="left">
+              Access these features when you get this pricing package business.
+            </Text>
+            <Stack as="ul" spacing="20px" pt="24px">
+              <ListItem>International calling and message API</ListItem>
+              <ListItem> Additional phone numbers</ListItem>
+              <ListItem>Automated messages via Zapier</ListItem>
+              <ListItem>24/7 support and consulting</ListItem>
+            </Stack>
+          </Box>
+        </Flex>
+      </Box>
     </Box>
   );
 }
